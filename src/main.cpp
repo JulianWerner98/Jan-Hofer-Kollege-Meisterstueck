ac2c5f42a72973a20ef786831b6383334cce84af
Julian Werner <info@werner-julian.de>
2022-02-15 21:19:47 +0100

    feat: add NOTAUS

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,18 @@
 #include <Arduino.h>
-#define STD_TIMER 640       //Schnellste Geschwindigkeit
-#define REDUCED_TIMER 12800 //Langsamste Geschwindigkeit
-#define VERLAMSARMUNG 1     //Wie schnell bremsen. Hohe Zahl = schnell bremsen
-#define STEP_PIN 6
+#define STD_TIMER 640      //Schnellste Geschwindigkeit
+#define REDUCED_TIMER 4000 //Langsamste Geschwindigkeit
+#define VERLAMSARMUNG 1    //Wie schnell bremsen. Hohe Zahl = schnell bremsen
+#define STEP_PIN 4
 #define DIR_PIN 7    //DIR -Direction
 #define ENABLE_PIN 8 //ENA-Enable
-#define LED_AUF 4    // Statusanzeige auf
-#define LED_ZU 5     // Statusanzeige zu
 #define LED_NANO 13  //LED des Nano's
 #define TASTER_ANSCHLAG_ZU 9
 #define TASTER_ANSCHLAG_AUF 10
 #define TASTER_BESCHLEUNIGUNG_AUF 2 //MUSS 2 ODER 3 SEIN
 #define TASTER_BESCHLEUNIGUNG_ZU 3  //MUSS 2 ODER 3 SEIN
-#define DIR_SCHLIESSEN 0            //Ändern wenn der Motor in die falsche Richtung fährt
-#define MOTOR_DREHT false           //Wenn Enable den Zustand hat ist er aktiv am fahren
+#define TASTER_NOTAUS 12
+#define DIR_SCHLIESSEN 0  //Ändern wenn der Motor in die falsche Richtung fährt
+#define MOTOR_DREHT false //Wenn Enable den Zustand hat ist er aktiv am fahren
 
 void beschAufInterrupt();
 void beschZuInterrupt();
@@ -29,9 +28,8 @@ void setup()
   pinMode(TASTER_ANSCHLAG_ZU, INPUT_PULLUP);
   pinMode(TASTER_BESCHLEUNIGUNG_AUF, INPUT_PULLUP);
   pinMode(TASTER_BESCHLEUNIGUNG_ZU, INPUT_PULLUP);
+  pinMode(TASTER_NOTAUS, INPUT_PULLUP);
 
-  pinMode(LED_AUF, OUTPUT);
-  pinMode(LED_ZU, OUTPUT);
   pinMode(LED_NANO, OUTPUT);
 
   pinMode(STEP_PIN, OUTPUT);
@@ -47,7 +45,7 @@ void setup()
   TCCR1A = 0;                               //Löschen des TCCR1A-Registers
   TCCR1B = 0;                               //Löschen des TCCR1B-Registers
   TCCR1B |= (1 << WGM12);                   //Setze CTC-Mode (Waveform Generation Mode)
-  TCCR1B |= /*(1 << CS11) | */ (1 << CS10); //Setze CS10 und CS11 (Clock Select), Vorteiler 1
+  TCCR1B |= /*(1 << CS11) | */ (1 << CS10); //Setze CS10 (Clock Select), Vorteiler 1
   TCNT1 = 0;                                //Timer Counter Register löschen
   OCR1A = STD_TIMER;                        //Vergleichswert 1249->200Hz, 12499-> 20Hz
   TIMSK1 |= (1 << OCIE1A);                  //Bit Output Compare A Match Interrupt Enable setzen
@@ -57,8 +55,6 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(TASTER_BESCHLEUNIGUNG_AUF), beschAufInterrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(TASTER_BESCHLEUNIGUNG_ZU), beschZuInterrupt, FALLING);
 
-  digitalWrite(LED_AUF, 0);
-  digitalWrite(LED_ZU, 0);
   digitalWrite(ENABLE_PIN, 0);
   digitalWrite(DIR_PIN, 0);
   digitalWrite(STEP_PIN, 0);
@@ -104,55 +100,59 @@ ISR(TIMER1_COMPA_vect)
     digitalWrite(LED_NANO, !digitalRead(LED_NANO));
   }
 
-  //Anschlag berührt
-  if (!digitalRead(TASTER_ANSCHLAG_AUF) || !digitalRead(TASTER_ANSCHLAG_ZU))
+  if (digitalRead(TASTER_NOTAUS))
   {
-    if (digitalRead(ENABLE_PIN) == MOTOR_DREHT)
-      Serial.println("Stoppe Motor");
-    digitalWrite(ENABLE_PIN, !MOTOR_DREHT);
-    faehrtAuf = false;
-    faehrtZu = false;
-    slowMode = false;
-    //Status LED
-    digitalWrite(LED_AUF, 0);
-    digitalWrite(LED_ZU, 0);
-  }
-  else
-  {
-    if (faehrtAuf)
+
+    //Anschlag berührt
+    if (!digitalRead(TASTER_ANSCHLAG_AUF) || !digitalRead(TASTER_ANSCHLAG_ZU))
     {
-      digitalWrite(DIR_PIN, !DIR_SCHLIESSEN);
-      digitalWrite(ENABLE_PIN, MOTOR_DREHT);
-      digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
-      //Status LED
-      digitalWrite(LED_AUF, 1);
-    }
-    else if (faehrtZu)
-    {
-      digitalWrite(ENABLE_PIN, MOTOR_DREHT);
-      digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
-      digitalWrite(DIR_PIN, DIR_SCHLIESSEN);
-      //Status LED
-      digitalWrite(LED_ZU, 1);
-    }
-    if (slowMode)
-    {
-      if (OCR1A + VERLAMSARMUNG < REDUCED_TIMER)
-      {
-        OCR1A += VERLAMSARMUNG;
-      }
-      else
-      {
-        if (OCR1A != REDUCED_TIMER)
-        {
-          Serial.println("Minial Geschwindigkeit");
-          OCR1A = REDUCED_TIMER;
-        }
-      }
+      if (digitalRead(ENABLE_PIN) == MOTOR_DREHT)
+        Serial.println("Stoppe Motor");
+      digitalWrite(ENABLE_PIN, !MOTOR_DREHT);
+      faehrtAuf = false;
+      faehrtZu = false;
+      slowMode = false;
     }
     else
     {
-      OCR1A = STD_TIMER;
+      if (faehrtAuf)
+      {
+        digitalWrite(DIR_PIN, !DIR_SCHLIESSEN);
+        digitalWrite(ENABLE_PIN, MOTOR_DREHT);
+        digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+      }
+      else if (faehrtZu)
+      {
+        digitalWrite(ENABLE_PIN, MOTOR_DREHT);
+        digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+        digitalWrite(DIR_PIN, DIR_SCHLIESSEN);
+      }
+      if (slowMode)
+      {
+        if (OCR1A + VERLAMSARMUNG < REDUCED_TIMER)
+        {
+          OCR1A += VERLAMSARMUNG;
+        }
+        else
+        {
+          if (OCR1A != REDUCED_TIMER)
+          {
+            Serial.println("Minial Geschwindigkeit");
+            OCR1A = REDUCED_TIMER;
+          }
+        }
+      }
+      else
+      {
+        OCR1A = STD_TIMER;
+      }
     }
+  }
+  else
+  {
+    digitalWrite(ENABLE_PIN, MOTOR_DREHT);
+    faehrtAuf = false;
+    faehrtZu = false;
+    slowMode = false;
   }
 }
